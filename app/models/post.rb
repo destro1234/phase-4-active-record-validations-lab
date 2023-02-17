ee99e3ee9cd9e300db23c1a67025217de16a8134
Elias <eliasventura23@gmail.com>
2023-02-16 20:52:40 -0500

    commit tree c706413c295c46d9e9a5512382345f0e0b857d0f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :click_bait
+
+    def click_bait
+        if self.title == "True Facts"
+            errors.add(:title, "This is clickbait!")
+        end
+    end
 end
+
